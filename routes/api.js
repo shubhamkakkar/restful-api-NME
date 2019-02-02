2dc25fd47addc6952e4bfd2236253e8fffe33219
shubhamkakkar <shubham,kakkar98@gmail.com>
2019-02-02 17:44:58 +0530

    restful api

---
+++ routes/api.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const Ninja = require("../models/ninja");
+
+// get a list of ninjas from the db
+router.get("/ninjas", (req, res, next) => {
+  Ninja.find({})
+    .then(ninjas => {
+      res.send(ninjas);
+    })
+    .catch(next);
+});
+
+// add a new ninja to the db
+router.post("/ninjas", (req, res, next) => {
+  Ninja.create(req.body)
+    .then(ninja => {
+      res.send(ninja);
+    })
+    .catch(next);
+});
+
+// update a ninja in the db
+router.put("/ninjas/:id", (req, res, next) => {
+  Ninja.findByIdAndUpdate({ _id: req.params.id }, req.body)
+    .then(() => {
+      Ninja.findOne({ _id: req.params.id }).then(ninja => {
+        res.send(ninja);
+      });
+    })
+    .catch(next);
+});
+
+// delete a ninja from the db
+router.delete("/ninjas/:id", (req, res, next) => {
+  Ninja.findByIdAndRemove({ _id: req.params.id })
+    .then(ninja => {
+      res.send(ninja);
+    })
+    .catch(next);
+});
+
+module.exports = router;
